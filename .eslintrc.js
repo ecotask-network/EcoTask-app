391ee435b1175818bf081b01bf987349157fdd8f
cybermax4200 <solapromise11@gmail.com>
2026-06-05 22:44:19 +0100

    ci: add RN bundle build job; add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: "@react-native",
+  rules: {
+    "react-native/no-unused-styles": 2,
+    "react-native/sort-styles": 0,
+    "react-native/no-inline-styles": 1,
+  },
+};
